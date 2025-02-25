327adf11adeacbfbefddb406adb7433148ea93f3
sabiss <sabrinabezsilva@gmail.com>
2025-02-25 15:04:08 -0300

    FEAT: lógica para envio de dados de component filho da nav

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { ListaDeTags } from '../../data/lista-tags';
 
 @Component({
   selector: 'app-navbar',
   standalone: false,
-  
+
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
-
+  @Output() tipoDeNotaSelecionada = new EventEmitter<string>();
+  listaTags: string[] = ListaDeTags; //essa lista so será usada no component TAG, filho de navbar, portanto, é melhor mantê-la aqui do que no AppComponent
+  enviarTipoDeNotaSelecionadaParaExibir(tag: string) {
+    this.tipoDeNotaSelecionada.emit(tag);
+  }
 }
