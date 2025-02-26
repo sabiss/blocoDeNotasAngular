1b28ebb4711b7a8cb015c916d6e7091408fb79cd
sabiss <sabrinabezsilva@gmail.com>
2025-02-26 10:24:12 -0300

    FEAT: botões de ação com a nota atual

---
+++ src/app/components/botoes-acoes-nota/botoes-acoes-nota.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-botoes-acoes-nota',
+  standalone: false,
+  
+  templateUrl: './botoes-acoes-nota.component.html',
+  styleUrl: './botoes-acoes-nota.component.scss'
+})
+export class BotoesAcoesNotaComponent {
+
+}
