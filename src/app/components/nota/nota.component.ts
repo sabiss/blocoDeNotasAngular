a9932c54a00be3c03a15244bb0a358ae59cf84f9
sabiss <sabrinabezsilva@gmail.com>
2025-02-26 10:22:07 -0300

    FEAT: compoent de nota

--- src/app/components/nota/nota.component.ts
+++ src/app/components/nota/nota.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { INota } from '../../interfaces/nota';
 
 @Component({
   selector: 'app-nota',
   standalone: false,
-  
+
   templateUrl: './nota.component.html',
-  styleUrl: './nota.component.scss'
+  styleUrl: './nota.component.scss',
 })
 export class NotaComponent {
+  @Input() listaDeNotasParaExibir: INota[] = [];
+  @Output() notaParaExibirDetalher = new EventEmitter<INota>();
 
+  selecionarNota(nota: INota) {
+    this.notaParaExibirDetalher.emit(nota);
+  }
 }
