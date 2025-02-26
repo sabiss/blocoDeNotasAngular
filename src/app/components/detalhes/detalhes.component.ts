a3052ff8d824729899cf616bed47b7a27e41b2ed
sabiss <sabrinabezsilva@gmail.com>
2025-02-26 10:23:29 -0300

    FEAT: component de detalhes sobre a nota selecionada

--- src/app/components/detalhes/detalhes.component.ts
+++ src/app/components/detalhes/detalhes.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { INota } from '../../interfaces/nota';
 
 @Component({
   selector: 'app-detalhes',
   standalone: false,
-  
+
   templateUrl: './detalhes.component.html',
-  styleUrl: './detalhes.component.scss'
+  styleUrl: './detalhes.component.scss',
 })
 export class DetalhesComponent {
-
+  @Input() notaParaExibirDetalhes: INota = {} as INota;
 }
