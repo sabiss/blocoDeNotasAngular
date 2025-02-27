7be9706ef37f1617886dc08a81ca48e464a5fe0c
sabiss <sabrinabezsilva@gmail.com>
2025-02-27 16:42:49 -0300

    FEAT:  formulário para criação de notas

---
+++ src/app/components/forms/forms.component.ts
@@ -0,0 +1,48 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { INota } from '../../interfaces/nota';
+
+@Component({
+  selector: 'app-forms',
+  standalone: false,
+
+  templateUrl: './forms.component.html',
+  styleUrl: './forms.component.scss',
+})
+export class FormsComponent {
+  @Input() tagsExistentes: string[] = [];
+  @Output() enviarNotaParaLista = new EventEmitter<INota>();
+  @Output() fecharForms = new EventEmitter<boolean>();
+
+  tituloNota: string = '';
+  textoNota: string = '';
+  dataNota: Date = new Date();
+  tagsSelecionadas: string[] = []; // Variável para armazenar o item selecionado
+
+  toggleTag(tag: string) {
+    const index = this.tagsSelecionadas.indexOf(tag);
+    if (index === -1) {
+      //a tag marcada ainda não estava no array, portanto, é adicionada
+      this.tagsSelecionadas.push(tag);
+    } else {
+      //o usuário desmarcou uma tag que antes estava marcada, então a tag é removida do array
+      this.tagsSelecionadas.splice(index, 1);
+    }
+  }
+
+  isChecked(tag: string): boolean {
+    //marca a tag se ela estiver no array das tags selecionadaas
+    return this.tagsSelecionadas.includes(tag);
+  }
+
+  criarNota() {
+    const nota: INota = {
+      titulo: this.tituloNota,
+      texto: this.textoNota,
+      tags: this.tagsSelecionadas,
+      data: this.dataNota,
+    };
+
+    this.enviarNotaParaLista.emit(nota);
+    this.fecharForms.emit(false);
+  }
+}
