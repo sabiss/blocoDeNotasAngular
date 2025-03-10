e7946b97a67a5b4c3d62d720a680bfbbd4175112
sabiss <sabrinabezsilva@gmail.com>
2025-03-10 10:41:06 -0300

    FEAT: add input para receber as notas que devem ser exibidas pelo component

--- src/app/components/navnotas/navnotas.component.ts
+++ src/app/components/navnotas/navnotas.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { INota } from '../../interfaces/nota';
 
 @Component({
   selector: 'app-navnotas',
   standalone: false,
-  
+
   templateUrl: './navnotas.component.html',
-  styleUrl: './navnotas.component.scss'
+  styleUrl: './navnotas.component.scss',
 })
 export class NavnotasComponent {
-
+  @Input() listaDeNotasParaExibir: INota[] = [];
 }
