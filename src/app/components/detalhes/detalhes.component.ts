01e163fb229f9afb3c21078378de2de4e0602af5
sabiss <sabrinabezsilva@gmail.com>
2025-03-12 16:33:35 -0300

    REFACTOR: trazendo responsabilidade dos botões de acao, função para fechar os detalhes e emissão da ação que deve ser feita na nota

--- src/app/components/detalhes/detalhes.component.ts
+++ src/app/components/detalhes/detalhes.component.ts
@@ -1,5 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { INota } from '../../interfaces/nota';
+import { AcaoNota } from '../../interfaces/acao-nota';
 
 @Component({
   selector: 'app-detalhes',
@@ -10,10 +11,20 @@ import { INota } from '../../interfaces/nota';
 })
 export class DetalhesComponent {
   @Input() notaParaExibirDetalhes: INota = {} as INota;
+  @Output() fecharDetalhes = new EventEmitter<boolean>();
+  @Output() arquivarDesarquivarOuExcluirNota = new EventEmitter<AcaoNota>();
+
+  fechar() {
+    this.fecharDetalhes.emit(false);
+  }
   rolarPraCima() {
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
     });
+  }
+
+  processarNota(acao: string, nota: INota) {
+    this.arquivarDesarquivarOuExcluirNota.emit({ acao: acao, nota: nota });
   }
 }
