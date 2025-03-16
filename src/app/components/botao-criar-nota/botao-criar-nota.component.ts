304e9253c714a13576a85e4ebe4d476e887cf001
sabiss <sabrinabezsilva@gmail.com>
2025-03-16 15:21:45 -0300

    REFACTOR: adicionando ajuste para reaproveitamento do component na versão DESKTOP

--- src/app/components/botao-criar-nota/botao-criar-nota.component.ts
+++ src/app/components/botao-criar-nota/botao-criar-nota.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { TelaService } from '../../services/tela/tela.service';
 
 @Component({
   selector: 'app-botao-criar-nota',
@@ -11,7 +12,15 @@ export class BotaoCriarNotaComponent {
   @Output() mudarStatusForm = new EventEmitter<boolean>();
   @Input() formsAberto: boolean = false;
 
+  changeBotaoCriar: boolean = true;
+
+  constructor(private telaService: TelaService) {}
+
   abrirForms() {
+    const telaPequena = this.telaService.telaPequena();
+    if (telaPequena) {
+      this.changeBotaoCriar = false;
+    }
     this.formsAberto = true;
     this.mudarStatusForm.emit(true);
   }
